1e58a2415b856fca7289f2652dfb31ee05589001
Lukashenkov, Viacheslav <viacheslav.lukashenkov@krontech.ru>
2018-09-19 09:25:14 +0300

    AITXT, AIALR messages using pynmea2 lib code

--- test_bl/test_trassa_plugin/structs_trassa/ai_alrm.py
+++ test_bl/test_trassa_plugin/structs_trassa/ai_alrm.py
@@ -1,10 +1,19 @@
-#import pynmea2
-
+from test_bl.test_trassa_plugin.structs_trassa.nmeautils import nmea
+from test_bl.test_trassa_plugin.structs_trassa.nmeautils.types import talker
 
 class AiAlrm():
     def __init__(self):
+        self.text_msg = talker.TXT('AI','TXT', ('1','1','21','External DGNSS in use'))
         return
 
     def set_values_from_map(self):
         return
-    
+
+def test_this():
+    ai_alr = AiAlrm()
+
+if __name__=="__main__":
+    test_this()
+
+
+
