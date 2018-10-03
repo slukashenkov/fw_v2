49189627616f2b5d9f86f27614331bbf93054a89
Lukashenkov, Viacheslav <viacheslav.lukashenkov@krontech.ru>
2018-10-03 12:07:54 +0300

    proper review change

--- test_bl/test_bl_tools/send_receive.py
+++ test_bl/test_bl_tools/send_receive.py
@@ -293,24 +293,24 @@ class SendReceive:
         curr_udp_server_name = self.udp_server_name + str(self.udp_servers_cnt)
         self.udp_servers_cnt = self.udp_servers_cnt + 1
         udp_server.name      = curr_udp_server_name
+        self.udp_servers[curr_udp_server_name] = udp_server
+
 
         udp_server = self.udp_servers.get(curr_udp_server_name)
         #self.udp_servers[curr_udp_server_name] = udp_server
-        #self.start_udp_server(curr_udp_server_name)
+        #self.start_udp_server(curr_udp_server_name)  def start_udp_server(self, srv_id):
+
         return curr_udp_server_name
 
     def start_udp_server(self,
                          srv_id):
-
-        if srv_id in self.udp_servers.keys():
             self.logger.info('Starting up UDP Server to listen traffic from KD')
-            curr_udp_srv = self.udp_servers[srv_id]
+            curr_udp_srv = self.udp_servers.get(srv_id)
             t = threading.Thread(target=curr_udp_srv.serve_forever)
             self.udp_servers_treads[srv_id]=t
             t.setDaemon(True)  # don't hang on exit
             t.start()
-        else:
-            raise Exception("UDP server is not KNOWN, check its ID")
+
 
 
 
