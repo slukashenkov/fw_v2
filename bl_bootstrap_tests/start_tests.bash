9084983130664d769b772c9d89307bb98050c9ac
slava lukashenkov <Viacheslav.Lukashenkov@krontech.ru>
2018-10-23 12:53:17 +0300

    delay for logserver start

--- bl_bootstrap_tests/start_tests.bash
+++ bl_bootstrap_tests/start_tests.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-export PYTHONPATH=/home/slon/tests_start/test_bl
-cd /home/slon/tests_start/test_bl/test_sonata_plugin/tests_sonata
-python3.6 test_loader_sonata.py
+export PYTHONPATH=/home/slon/Bl_FW_V2
+echo $PYTHONPATH
+
+#python3.6 test_loader_sonata.py
+python3.6 /home/slon/Bl_FW_V2/test_bl/test_sonata_plugin/tests_sonata/test_sonata.py
