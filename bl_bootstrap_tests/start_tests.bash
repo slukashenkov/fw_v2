bd7b1f91255a633ed79b35faef7e76c45c84ff9e
slava lukashenkov <Viacheslav.Lukashenkov@krontech.ru>
2018-10-23 13:12:14 +0300

    set DISPLAY for chainsaw

--- bl_bootstrap_tests/start_tests.bash
+++ bl_bootstrap_tests/start_tests.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export PYTHONPATH=/home/slon/Bl_FW_V2
-echo $PYTHONPATH
+set DISPLAY=:0
 
 #python3.6 test_loader_sonata.py
 python3.6 /home/slon/Bl_FW_V2/test_bl/test_sonata_plugin/tests_sonata/test_sonata.py
